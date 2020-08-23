f3dfd8be83d2b2b7676d5b4ec56dc3f1bef2de73
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-23 22:34:20 +0300

    made Tree and forest objects

---
+++ Forest/Forest/Forest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forest
+{
+    public class Forest
+    {
+        public Tree[,] Trees { get; set; }
+        public Forest(int length, int weight, int milisecondsIterrations)
+        {
+            Trees = new Tree[weight, length];
+            for (int i = 0; i < weight; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Trees[i, j] = new Tree();
+                }
+            }
+        }
+
+        public void MAkeConnection(double chance)
+        {
+            for (int i = 0; i < Trees.GetLength(0); i++)
+            {
+                for (int j = 0; j < Trees.GetLength(1); j++)
+                {
+                    Random rander = new Random();
+                    int rand = rander.Next(1, 101);
+                    if (chance >= rand)
+                    {
+                        Trees[i, j].Connected = Trees[i, j + 1]; //ToDo: make validation on array index
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Forest/Forest/Tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forest
+{
+    public enum TreeState 
+    {
+        health = 'o',
+        burned = 'x',
+        dead = '.'
+    }
+    public class Tree
+    {
+        public TreeState State { get; set; }
+        public int Health { get; set; }
+        public Tree Connected { get; set; }
+        public Tree()
+        {
+            State = TreeState.health;
+            Health = 3;
+        }
+        public void Burn()
+        {
+            State = TreeState.burned;
+        }
+    }
+}
